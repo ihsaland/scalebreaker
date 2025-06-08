441330764534fd7a5b474e8e9ae216c2e4c224da
iansalandy <ihsaland@gmail.com>
2025-06-07 22:43:31 -0400

    Add Google Analytics implementation with Measurement ID G-50931S8RQ1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import Automata from './components/Automata'
-import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import Automata from './components/Automata';
+import GoogleAnalytics from './components/GoogleAnalytics';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Automata />
+    <BrowserRouter>
+      <div className="App">
+        <GoogleAnalytics />
+        <Automata />
       </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
